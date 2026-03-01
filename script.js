e00da1f8e1b8e121b8eae6f87a36289190e73571
zombad666-dotcom <zombad666@gmail.com>
2026-03-01 21:40:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "sk-8k5UKTJ_dFUHgXPnb_1v4Q";
+const API_KEY = "";
 
 async function sendMessage() {
 
@@ -51,6 +51,7 @@ async function sendMessage() {
 
     chat.scrollTop = chat.scrollHeight;
 }
+
 
 
 
